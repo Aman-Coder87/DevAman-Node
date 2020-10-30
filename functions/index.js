1479a0bea8bcb99725c7470e2a0e79c05f4a0051
Aman-Coder87 <webspyder.net@gmail.com>
2020-10-30 09:58:05 +0545

    sample express codes

---
+++ functions/index.js
@@ -0,0 +1,54 @@
+const functions = require('firebase-functions');
+// Importing Npm Packages //
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+const os = require('os');
+const http = require('http');
+const fs = require('fs');
+const chalk = require('chalk');
+
+// App Envirement Setup //
+const app = express();
+const port = process.env.PORT || 8080 ;
+
+// Path Decleration | Path that Goes in Public And Templates Folder //
+const staticPath = path.join( __dirname , '/public');
+const templatesPath = path.join(__dirname, '/templates/views');
+
+// Using App To Preform Tasks //
+app.use(express.static(staticPath));
+// set hbs view engine in express //
+app.set('view engine', 'hbs');
+app.set('views', templatesPath);
+// partials regiatering process //
+hbs.registerPartials(__dirname + '/templates/partials');
+
+// App Page Routing //
+// Home Page //
+app.get('/' , (req , res) => {
+    res.sendFile(index.html);
+});
+
+// About Page //
+app.get('/about' , (req , res) => {
+    res.set('Cache-Control' , 'public , max-age=300 , s-,masage=600');
+    res.sendFile(path.join(staticPath , '/about.html'));
+});
+
+// Contact Page //
+app.get('/contact' , (req , res) => {
+    res.sendFile(contact.html);
+});
+
+// 404 Error Page //
+app.get('*' , (req , res) => {
+    res.render('404');
+});
+
+// App Listining To Server //
+app.listen(port , () => {
+    console.log(`${chalk.red.bgBlue('App is listining on')}   ${chalk.red.bgBlue.bold('http://127.0.0.1:8080')}`);
+});
+
+exports.app = functions.https.onRequest(app);
---
+++ public/Js/main.js
@@ -0,0 +1,5 @@
+
+
+ScrollOut({
+  targets: 'h1, h2, h3, h4, h5, p,img,ul,a,div'
+});
